cdc71fae62bea696cfd5ef3a993cfde0a1f008b8
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-11 22:35:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,7 +13,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def initialize(brand)
+  def brand=(brand)
     @brand = brand
     if !(BRANDS.include?(@brand))
       BRANDS << @brand
